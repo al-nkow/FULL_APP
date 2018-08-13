476f72f2ad999c30b388aa07dd186e62463719a5
Aleksey <akovalchuk@bekitzur.com>
2018-08-13 18:11:18 +0300

    0.0.0.0

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`=== Server started on ${port} ===`));
+server.listen(port, '0.0.0.0', () => console.log(`=== Server started on ${port} ===`));
