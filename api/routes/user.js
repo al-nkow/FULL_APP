33a06d08bd5fbf62dfb7e9ce906005269b167364
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-10-07 19:32:26 +0300

    new admin

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,18 +10,9 @@ const passportJWT = passport.authenticate('jwt', { session: false });
 router.get('/', passportJWT, UsersController.users_get_user);
 router.post('/signup', passportJWT, UsersController.users_user_signup);
 router.post('/login', UsersController.users_user_login);
-router.delete('/:userId', passportJWT, UsersController.users_user_delete);
-
 router.post('/password', passportJWT, UsersController.users_user_change_password);
-
 router.post('/token', UsersController.users_user_token);
-
-router.get('/protected', passportJWT, (req, res) => {
-  res.send('I\'m protected!');
-});
-
 router.post('/logout', passportJWT, UsersController.users_user_logout);
-
-
+router.delete('/:userId', passportJWT, UsersController.users_user_delete);
 
 module.exports = router;
