e8682e7874fd4dab13225719ff5b08ad5e053032
Aleksey Kovalchuk <al.nkow@gmail.com>
2019-05-01 22:25:39 +0300

    404 error page

--- app.js
+++ app.js
@@ -133,6 +133,23 @@ app.get('/', async (req, res) => {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  // if (req.accepts('json')) {
+  //   res.send({ error: 'Not found' });
+  //   return;
+  // }
+
+  // default to plain-text. send()
+  // res.type('txt').send('Not found');
+
   const error = new Error('Not found');
   error.status = 404;
   next(error);
